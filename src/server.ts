8677e7b04055c0960811bd9e22741bd4a45770a4
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-05-16 16:29:51 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import 'express-async-errors';
 
+import 'express-async-errors';
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 
 import AppError from './errors/AppError';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
