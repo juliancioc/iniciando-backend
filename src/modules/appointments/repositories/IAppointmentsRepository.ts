f2bb566e61f87718c92cdac696831843d6c34694
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-05-29 09:18:54 -0300

    config repository

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,6 @@
 import Appointment from '../infra/typeorm/entities/Appoinment';
-
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 export default interface IAppointmentsRepository {
+    create(date: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
 }
