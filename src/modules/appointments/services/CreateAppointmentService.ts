7fa5608879af2caf04e58e50aa8aa83bfd5f5303
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-05-29 23:17:04 -0300

    solid dependency inversion principle

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,28 +1,29 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appoinment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-interface Request {
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+interface IRequest {
     provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ date, provider_id }: Request): Promise< Appointment> {
-        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    constructor(private appointmentsRepository: IAppointmentsRepository){}
+
+    public async execute({ date, provider_id }: IRequest): Promise< Appointment> {
         const appointmentDate = startOfHour(date);
-        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
-        )
+        );
 
         if (findAppointmentInSameDate) {
             throw new AppError('This appoinment is already booked');
         }
 
-        const appointment = await appointmentsRepository.create({
+        const appointment = await this.appointmentsRepository.create({
             provider_id,
             date: appointmentDate,
         });
