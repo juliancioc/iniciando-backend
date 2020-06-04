3474f18141cb6796c1a4a6e82b55b9f3a7f43a9f
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-06-03 22:45:24 -0300

    tests finished

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -36,3 +36,53 @@ describe('AuthenticateUser', () => {
         expect(response.user).toEqual(user);
     });
 });
+
+describe('AuthenticateUser', () => {
+    it('should not be able to authenticate with non existing user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();;
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        );
+
+        expect(
+            authenticateUser.execute({
+                email: 'julianciocarvalho@example.com',
+                password: '123456'
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
+
+describe('AuthenticateUser', () => {
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        );
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        );
+
+
+        await createUser.execute({
+            name: 'Juliancio Carvalho',
+            email: 'julianciocarvalho@example.com',
+            password: '123456'
+        });
+
+        expect(
+            authenticateUser.execute({
+                email: 'julianciocarvalho@example.com',
+                password: 'wrong-password',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
