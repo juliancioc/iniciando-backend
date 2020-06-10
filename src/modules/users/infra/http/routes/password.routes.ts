0d97b4c2b941f74224f0e1f151fee0c5a6f54779
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-06-10 20:51:59 -0300

    create database reset and forget password

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
-import SessionsController from '../controllers/SessionsController';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
-const sessionsController = new SessionsController();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/', sessionsController.create);
+passwordRouter.post('/forgot', forgotPasswordController.create);
+passwordRouter.post('/reset', resetPasswordController.create);
 
 export default passwordRouter;
