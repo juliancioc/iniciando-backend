7486e97fdf0f913a67a58378b9640ad2124def55
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-06-21 13:42:23 -0300

    create service provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -9,7 +9,6 @@ import EtherealMailProvider from './MailProvider/implementations/EtherealMailPro
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
-
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
