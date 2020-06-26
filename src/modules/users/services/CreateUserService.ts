7a27aeac604f331988e6e8776835431ab6272b3c
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-06-26 09:38:24 -0300

    cache providers

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,7 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '../infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 interface IRequest {
     name: string;
     email: string;
@@ -19,6 +20,9 @@ class CreateUserService {
 
         @inject('HashProvider')
         private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) { }
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
