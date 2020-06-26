43832bc57e9fc85afc3551352dcaee59e280f858
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-06-26 00:20:18 -0300

    config redis

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import './CacheProvider';
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import S3StorageProvider from './StorageProvider/implementations/S3StorageProvider';
