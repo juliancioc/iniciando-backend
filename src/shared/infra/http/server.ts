69e8513de4e6c79c5a502993ee2ebb61bb49243d
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-07-01 11:32:44 -0300

    express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,12 +9,14 @@ import express, { Request, Response, NextFunction } from 'express';
 import AppError from '../../errors/AppError';
 import routes from './routes';
 import uploadConfig from '@config/upload';
+import rateLimiter from '../middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder));
