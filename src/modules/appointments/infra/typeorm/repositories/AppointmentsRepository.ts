8434d2af3314e893517964e9baed010cadba6772
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-07-07 09:27:44 -0300

    adjusts

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -29,6 +29,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
                         `to_char(${dateFieldName}, 'MM-YYYY') = '${parseMonth}-${year}'`,
                 ),
             },
+            relations: ['user']
         });
 
         return appointments;
@@ -56,9 +57,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
         return appointments;
     }
 
-    public async findByDate(date: Date): Promise<Appointment | undefined> {
+    public async findByDate(
+        date: Date,
+        provider_id: string
+    ): Promise<Appointment | undefined> {
         const findAppointment = await this.ormRepository.findOne({
-            where: { date },
+            where: { date, provider_id },
         });
 
         return findAppointment || undefined;
@@ -69,7 +73,10 @@ class AppointmentsRepository implements IAppointmentsRepository {
         user_id,
         date,
     }: ICreateAppointmentDTO): Promise<Appointment> {
-        const appointment = this.ormRepository.create({ provider_id, user_id, date });
+        const appointment = this.ormRepository.create({ 
+            provider_id, 
+            user_id, date 
+        });
 
         await this.ormRepository.save(appointment);
 
